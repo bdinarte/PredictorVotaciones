63ae972abae68f4b7e45663fafa411b3f217aa96
Brandon <bdinarte@yahoo.com>
2018-04-24 23:12:36 -0600

    normalizacion con pandas

--- tec/ic/ac/p1/modelo/normalizacion.py
+++ tec/ic/ac/p1/modelo/normalizacion.py
@@ -1,1 +1,32 @@
 
+import pandas as pd
+
+
+def normalize(data, attribute_to_norm, scale_function):
+
+    f = feature_scaling
+    if scale_function == 'ss':
+        f = standard_scaling
+    if scale_function == 'os':
+        f = overmax_scaling
+
+    for col in attribute_to_norm:
+        data[col] = f(data[col])
+
+
+def feature_scaling(df):
+
+    # x_normalized = (x - xmin) / (xmax - xmin)
+    return (df - df.min()) / (df.max - df.min)
+
+
+def standard_scaling(df):
+
+    # x_normalized = (x - xmean) / xstddev
+    return (df - df.mean()) / df.std
+
+
+def overmax_scaling(df):
+
+    # x_normalized = x / xmax
+    return df - df.max()
