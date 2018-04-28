5b5af32b11756434ccdb108bb438740bd74457f3
Brandon <bdinarte@yahoo.com>
2018-04-28 14:11:32 -0600

    regersion logistica
    build_model_columns
    parse_csv
    removida tilde 'alfabetizacion'

--- tec/ic/ac/p1/modelo/regresion_logistica.py
+++ tec/ic/ac/p1/modelo/regresion_logistica.py
@@ -9,7 +9,7 @@ _CSV_COLUMN_DEFAULTS = [[0], [''], [0], [''], [''], [''], [''], [''], [''],
 
 _CSV_COLUMNS = ['VOTANTE', 'CANTON', 'EDAD', 'ES URBANO', 'SEXO',
                 'ES DEPENDIENTE', 'ESTADO VIVIENDA', 'E.HACINAMIENTO',
-                'ALFABETIZACIÓN', 'ESCOLARIDAD PROMEDIO',
+                'ALFABETIZACION', 'ESCOLARIDAD PROMEDIO',
                 'ASISTENCIA EDUCACION', 'FUERZA DE TRABAJO', 'SEGURO',
                 'N.EXTRANJERO', 'C.DISCAPACIDAD', 'POBLACION TOTAL',
                 'SUPERFICIE', 'DENSIDAD POBLACION',
@@ -20,18 +20,63 @@ _SHUFFLE_BUFFER = 1000
 
 data = obtener_dataframe('../archivos/pruebas.csv')
 
-atributos_a_normalizar = ['EDAD', 'ESCOLARIDAD PROMEDIO', 'POBLACION TOTAL',
-                          'SUPERFICIE', 'DENSIDAD POBLACION',
-                          'VIVIENDAS INDIVIDUALES OCUPADAS',
-                          'PROMEDIO DE OCUPANTES', 'P.JEFAT.FEMENINA',
-                          'P.JEFAT.COMPARTIDA']
+numeric_cols = ['EDAD', 'ESCOLARIDAD PROMEDIO', 'POBLACION TOTAL',
+                'SUPERFICIE', 'DENSIDAD POBLACION',
+                'VIVIENDAS INDIVIDUALES OCUPADAS', 'PROMEDIO DE OCUPANTES',
+                'P.JEFAT.FEMENINA', 'P.JEFAT.COMPARTIDA']
 
-data = normalize(data, atributos_a_normalizar, 'os')
+data = normalize(data, numeric_cols, 'os')
 
 print(data)
 
+
 def __build_model_columns():
-    pass
+
+    tf_numeric = tf.feature_column.numeric_column
+    # Columnas de valores numericos
+    edad = tf_numeric('EDAD')
+    escolaridad = tf_numeric('ESCOLARIDAD PROMEDIO')
+    poblacion = tf_numeric('POBLACION TOTAL')
+    superficie = tf_numeric('SUPERFICIE')
+    densidad = tf_numeric('DENSIDAD POBLACION')
+    viviendas_ocupadas = tf_numeric('VIVIENDAS INDIVIDUALES OCUPADAS')
+    ocupantes = tf_numeric('PROMEDIO DE OCUPANTES')
+    p_jefat_fem = tf_numeric('P.JEFAT.FEMENINA')
+    p_jefat_com = tf_numeric('P.JEFAT.COMPARTIDA')
+
+    tf_categoric = tf.feature_column.categorical_column_with_vocabulary_list
+    # Columnas de valores categorizables
+    urbano = tf_categoric(_CSV_COLUMNS[3], ['NO URBANO', 'URBANO'])
+    genero = tf_categoric(_CSV_COLUMNS[4], ['M', 'F'])
+    est_vivienda = tf_categoric(_CSV_COLUMNS[6], ['V. MAL ESTADO',
+                                                  'V. BUEN ESTADO'])
+    hacinamiento = tf_categoric(_CSV_COLUMNS[7], ['V. NO HACINADA',
+                                                  'V. HACINADA'])
+    alfabetismo = tf_categoric(_CSV_COLUMNS[8], ['NO ALFABETIZADO',
+                                                 'ALFABETIZADO'])
+    educacion = tf_categoric(_CSV_COLUMNS[10], ['EDUCACION REGULAR INACTIVA',
+                                                'EN EDUCACION REGULAR'])
+    trabajo = tf_categoric(_CSV_COLUMNS[11], ['DESEMPLEADO', 'EMPLEADO'])
+    seguro = tf_categoric(_CSV_COLUMNS[12], ['NO ASEGURADO', 'ASEGURADO'])
+    extranjero = tf_categoric(_CSV_COLUMNS[13], ['NO EXTRANJERO',
+                                                 'EXTRANJERO'])
+    discapacidad = tf_categoric(_CSV_COLUMNS[14], ['NO DISCAPACITADO',
+                                                   'DISCAPACITADO'])
+
+    # Columnas de valores categorizados desconocidos
+    canton = tf.feature_column.categorical_column_with_hash_bucket(
+        'CANTON', hash_bucket_size=1000)
+
+    # Agrupando columnas segun su tipo de dato
+    numeric_tf_cols = [edad, escolaridad, poblacion, superficie, densidad,
+                       viviendas_ocupadas, ocupantes, p_jefat_fem, p_jefat_com]
+    categoric_tf_cols = [urbano, genero, est_vivienda, hacinamiento,
+                         alfabetismo, educacion, trabajo, seguro, extranjero,
+                         discapacidad, canton]
+
+    # TODO: quizas definir crossed_column entre canton y sus indicadores
+
+    return numeric_tf_cols + categoric_tf_cols
 
 
 def entrenar(datos, regularizacion):
@@ -40,13 +85,11 @@ def entrenar(datos, regularizacion):
 
 def __input_fn(data_file, num_epochs, shuffle, batch_size):
 
-    def parse_csv(value):
-        columns = tf.decode_csv(value, record_defaults=_CSV_COLUMN_DEFAULTS)
-        features = dict(zip(_CSV_COLUMNS, columns))
-        labels = features.pop('VOTO_R2')
-        labels = features.pop('VOTO_R1')
-
-        return features, labels
+    def parse_csv(line):
+        parsed_line = tf.decode_csv(line, _CSV_COLUMN_DEFAULTS)
+        _features = tf.reshape(parsed_line[2:-2], shape=(20,))
+        label = tf.reshape(parsed_line[-2], shape=())
+        return _features, label
 
     dataset = tf.data.TextLineDataset(data_file)
 
