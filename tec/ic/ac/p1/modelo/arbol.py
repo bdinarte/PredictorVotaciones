a28721e24c73644b6552a5dc04f8a6e07a6af917
Armando López C <armando12101996@gmail.com>
2018-04-28 22:31:30 -0600

    Determinar mejor atributo para bifurcación
    Se crean algunas funciones y clases para determinar el mejor atributo de un set de datos, para crear así una bifurcación en el árbol de decisión.

--- tec/ic/ac/p1/modelo/arbol.py
+++ tec/ic/ac/p1/modelo/arbol.py
@@ -1,4 +1,110 @@
-import math
+
+from math import log
+from texttable import Texttable
+
+
+class DataSet:
+    def __init__(self, data):
+        self._data = data
+        self._entropy = 0
+        for featureValue in Feature((data[0].__len__() - 1), data).get_values():
+            self._entropy += self.minus_plog_2(featureValue.get_occurences() / data.__len__() - 1)
+
+    def get_entropy(self):
+        return self._entropy
+
+    def get_data(self):
+        return self._data
+
+    def minus_plog_2(self, p):
+        return_value = 0
+        p = abs(p)
+        if p != 0:
+            return_value = (-1) * p * log(p, 2)
+        return return_value
+
+    def __str__(self):
+        table = Texttable()
+        table.header(self._data[0])
+        for i in range(1, self._data.__len__()):
+            table.add_row(self._data[i])
+        return table.draw().__str__()
+
+
+class Feature:
+    def __init__(self, column, data):
+        self._name = data[0][column]
+        self._values = set()
+
+        for row in range(1, data.__len__()):
+            self._values.add(FeatureValue(data[row][column]))
+        for featureValue in self._values:
+            counter = 0
+            for row in range(1, data.__len__()):
+                if featureValue.get_name() == data[row][column]:
+                    counter += 1
+                    featureValue.set_occurences(counter)
+
+    def get_values(self):
+        return self._values
+
+    def get_name(self):
+        return self._name
+
+    def __str__(self):
+        return self._name
+
+
+class FeatureValue:
+    def __init__(self, name):
+        self._name = name
+        self._occurences = 0
+
+    def __eq__(self, other):
+        return (isinstance(other, FeatureValue)) and (other._name == self._name)
+
+    def __hash__(self):
+        return hash(self._name)
+
+    def get_name(self):
+        return self._name
+
+    def get_occurences(self):
+        return self._occurences
+
+    def set_occurences(self, occurences):
+        self._occurences = occurences
+
+    def __str__(self):
+        return self._name
+
+
+# ---------------------------------------------------------------------
+
+def create_data_set(featureValue, column, data):
+    returnData = [[FeatureValue for i in range(data[0].__len__())]
+              for j in range(featureValue.get_occurences() + 1)]
+
+    returnData[0] = data[0]
+    counter = 1
+
+    for row in range(1, data.__len__()):
+        if data[row][column] == featureValue.get_name():
+            returnData[counter] = data[row]
+            counter += 1
+    return DataSet([row[:column] + row[column+1:] for row in returnData])
+
+
+# ---------------------------------------------------------------------
+
+def generate_info_gain_table(featuresInfoGain):
+    table = Texttable()
+    table.header(['Feature', 'Information Gain'])
+
+    for key in featuresInfoGain:
+        table.add_row([key, round(featuresInfoGain[key], 5)])
+
+    return table.draw().__str__()
 
 
 # ---------------------------------------------------------------------
@@ -13,7 +119,7 @@ def entropia(q):
     :return: valor de entropia de la variable evaluada
     """
 
-    valor_entropia = -(q * math.log(q, 2) + (1-q) * math.log(1-q, 2))
+    valor_entropia = -(q * log(q, 2) + (1-q) * log(1-q, 2))
 
     return valor_entropia
 
@@ -72,7 +178,7 @@ def porporcion_positivos(muestra):
 # ---------------------------------------------------------------------
 
 array = [
-    [1,2,3,4,5,True],
+    ['a','s','d','f','g','Meta'],
     [1,1,3,4,5,False],
     [1,9,1,4,8,True],
     [1,2,3,3,4,False],
@@ -96,7 +202,39 @@ ie_rest = [
 ]
 
 
-q = porporcion_positivos(ie_rest)
-print("Proporcion posiivos ", q)
+muestra_prueba = [
+    ['PROVINCIA', 'TRABAJADOR', 'EDAD', 'SOLTERO', 'VOTO'],
+    ['SAN JOSE', 'SI', '25', 'SI', 'RES'],
+    ['CARTAGO', 'SI', '22', 'NO', 'PAC'],
+    ['HEREDIA', 'SI', '50', 'NO', 'PAC'],
+    ['HEREDIA', 'NO', '70', 'SI', 'LIB'],
+    ['HEREDIA', 'SI', '30', 'NO', 'PAC'],
+    ['CARTAGO', 'NO', '18', 'SI', 'RES'],
+    ['HEREDIA', 'NO', '20', 'NO', 'PAC'],
+]
 
-print("Entropia ", entropia(q))
+datas = {'Random': muestra_prueba}
+
+for key in datas:
+    print(key, 'DATASET:')
+    dataSet = DataSet(datas[key])
+    print(dataSet)
+    featuresInfoGain = {}
+
+    for column in range(0, dataSet.get_data()[0].__len__() - 1):
+        feature = Feature(column, dataSet.get_data())
+        dataSets = [DataSet for i in range(feature.get_values().__len__())]
+
+        i = 0
+        for featureValue in  feature.get_values():
+            dataSets[i] = create_data_set(featureValue, column, dataSet.get_data())
+            i += 1
+
+        summation = 0
+        for i in range(0, dataSets.__len__()):
+            summation += (((dataSets[i].get_data()).__len__() - 1) /
+                          (dataSet.get_data().__len__() - 1)) * dataSets[i].get_entropy()
+        featuresInfoGain[feature] = dataSet.get_entropy() - summation
+
+    print(generate_info_gain_table(featuresInfoGain))
+    print("\nBest feature to split on is: ", max(featuresInfoGain, key=featuresInfoGain.get), "\n")
