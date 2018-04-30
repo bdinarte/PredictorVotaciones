2fda8783c899f40835fbce31b05defb39cccf0ce
Brandon <bdinarte@yahoo.com>
2018-04-29 19:32:30 -0600

    inicio

--- tec/ic/ac/p1/modelo/red_neuronal.py
+++ tec/ic/ac/p1/modelo/red_neuronal.py
@@ -1,1 +1,25 @@
 
+from pandas import DataFrame
+
+from tec.ic.ia.pc1.g03 import generar_muestra_pais
+data_columns = ['CANTON', 'EDAD', 'ES_URBANO', 'SEXO', 'ES_DEPENDIENTE',
+                'ESTADO_VIVIENDA', 'E.HACINAMIENTO', 'ALFABETIZACION',
+                'ESCOLARIDAD_PROMEDIO', 'ASISTENCIA_EDUCACION',
+                'FUERZA_DE_TRABAJO', 'SEGURO', 'N.EXTRANJERO',
+                'C.DISCAPACIDAD', 'POBLACION_TOTAL', 'SUPERFICIE',
+                'DENSIDAD_POBLACION', 'VIVIENDAS_INDIVIDUALES_OCUPADAS',
+                'PROMEDIO_DE_OCUPANTES', 'P.JEFAT.FEMENINA',
+                'P.JEFAT.COMPARTIDA', 'VOTO_R1', 'VOTO_R2']
+
+
+def red(data_list):
+
+    data = DataFrame(data_list, columns=data_columns)
+    data.head()
+
+
+def main():
+    train_data = generar_muestra_pais(100)
+
+    _red = red(train_data)
+
