7769769078e89ca1f6deea0efec3945bf9a73a43
Julian Salinas <julian4001@outlook.com>
2018-05-03 01:48:14 -0600

    Documentando g03.py

--- tec/ic/ia/p1/g03.py
+++ tec/ic/ia/p1/g03.py
@@ -16,26 +16,25 @@ def obtener_argumentos():
     Se leen los comandos que se ingresaron en el cmd
 
     NOTAS:
+
         ♣ Al parsear los argumentos, recordar que si no se colocaron,
-          por defecto se quedan en 'false' o en 'None' en caso de
-          que sean 'int', 'float' o 'str'.
+        por defecto se quedan en 'False' si se trata de un valor bool.
+        En caso de que sean 'int', 'float' o 'str', el default es 'None'
 
         ♣ Todos los argumentos 'str' o 'int' se retornan como una lista
-          de un solo elemento.
+        de un solo elemento.
 
-        ♣ El 'action=store_true' significa que se almacena un
-          'true' si el comando se encuentra. No es necesario añadir un
-           valoral lado del argumento
+        ♣ El 'action=store_true' significa que se almacena un 'true'
+        si el comando se encuentra. No es necesario añadir un
+        valoral lado del argumento
 
-   :return: Namespace con los valores
+   :return: Namespace con los argumentos
    """
 
     parser = argparse.ArgumentParser()
-
-    # Globales
     parser.add_argument('--prefijo', nargs=1, type=str)
-    parser.add_argument('--poblacion', nargs=1, type=int)
     parser.add_argument('--provincia', nargs=1, type=str)
+    parser.add_argument('--poblacion', nargs=1, type=int, default=1000)
     parser.add_argument('--porcentaje-pruebas', nargs=1, type=int, default=10)
 
     # Opcional. Cantidad de segmentos en k-fold-cross-validation
@@ -47,16 +46,12 @@ def obtener_argumentos():
     parser.add_argument('--l2', action='store_true')
 
     # Redes neuronales
-    # En sus argumentos se debe colocar un valor después de las banderas
-    # TODO: Se deben colocar cuáles son los valores válidos
     parser.add_argument('--red-neuronal', action='store_true')
     parser.add_argument('--numero-capas', nargs=1, type=int)
     parser.add_argument('--unidades-por-capa', nargs=1, type=int)
     parser.add_argument('--funcion-activacion', nargs='*')
 
     # Árboles de decisión
-    # Ganancia de información mínima para realizar la partición
-    # en la poda del árbol
     parser.add_argument('--arbol', action='store_true')
     parser.add_argument('--umbral-poda', nargs=1, type=float)
 
@@ -71,21 +66,26 @@ def obtener_argumentos():
 
 def obtener_datos(args):
     """
-    Se usa el simulador para obtener la lista de listas
+    Se usa el simulador para obtener la lista de votantes
     :param args: Argumentos de la línea de comandos
     :return: Lista de listas donde cada una es un votante
     """
 
+    # Si no se específica por defecto es 1000
+    poblacion = args.poblacion[0]
+
+    # Provincias válidas, pueden ingresarsen en minúscula
     provincias = ['CARTAGO', 'SAN JOSE', 'LIMON',
                   'PUNTARENAS', 'GUANACASTE', 'ALAJUELA', 'HEREDIA']
 
+    # Si no se específica, se generan muestras del país
     if args.provincia is None:
-        return np.array(generar_muestra_pais(args.poblacion[0]))
+        return np.array(generar_muestra_pais(poblacion))
 
     provincia = args.provincia[0].upper()
 
     if provincias.__contains__(provincia):
-        return np.array(generar_muestra_provincia(args.poblacion[0], provincia))
+        return np.array(generar_muestra_provincia(poblacion, provincia))
 
     print('La provincia especificada no existe')
     exit(-1)
@@ -94,12 +94,20 @@ def obtener_datos(args):
 
 
 def main():
+    """
+    Obtiene los argumentos, genera los datos con el simulador y
+    ejecuta el modelo seleccionado según dichos argumentos.
+    :return:
+    """
+
     args = obtener_argumentos()
     datos = obtener_datos(args)
     np.random.shuffle(datos)
 
     if args.knn:
         analisis_knn(args, datos)
+
+    # TODO: Agregar aquí los demás 'if'
 
 # -----------------------------------------------------------------------------
 
